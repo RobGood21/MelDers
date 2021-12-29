050e0cf1f66218f8815f12105c50afc428ddd552
Rob Antonisse <robantonisse@gmail.com>
2021-12-29 12:08:18 +0100

    Begin

--- MelDers/MelDers/MelDers.ino
+++ MelDers/MelDers/MelDers.ino
@@ -1,7 +1,12 @@
 /*
  Name:		MelDers.ino
  Created:	12/29/2021 12:05:23 PM
- Author:	gebruiker
+ Author:	Rob Antonisse
+
+ Modelspoor terugmelding via S88
+
+
+
 */
 
 // the setup function runs once when you press reset or power the board
