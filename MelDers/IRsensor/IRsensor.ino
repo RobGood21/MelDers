5049d5fc9890f37ed4d8b108fb0a1a27fa6b8a8c
Rob Antonisse <robantonisse@gmail.com>
2022-04-30 11:18:23 +0200

    LaserSensor

--- MelDers/IRsensor/IRsensor.ino
+++ MelDers/IRsensor/IRsensor.ino
@@ -1,15 +1,64 @@
 /*
  Name:		IRsensor.ino
  Created:	4/15/2022 9:25:52 AM
- Author:	gebruiker
+ Author:	RobAntonisse
+
+ IR sensoren hebben veel last van zonlicht.
+ Test project om te zien of er algoritmische oplossingen zijn te maken om dit verschijnsel tegen te gaan.
+
+
+
 */
 
-// the setup function runs once when you press reset or power the board
+
+
+//tijdelijke variabele
+unsigned long tt;
+int bt = 500; //bt=brandtijd
+bool sensoraan;
+//GPIOR0 bit0 =leled aan/uit
+
 void setup() {
+	Serial.begin(9600);
+	//poorten instellen
+	DDRB |= (1 << 0); //pin8 output controle led
+	DDRB |= (1 << 1);// PORTB |= (1 << 1);//pin9 output TX led, temp constant aan
+
+	PORTC |= (1 << 0); //pullup op pin A0, is al input
+
 
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
-  
+	//gpior0 bit0=txled aan of uit
+	bool sensor = false;
+
+	if (millis() - tt > 0) { //iedere millisec
+		tt = millis();
+		if (PINC & (1 << 0)) sensor = true;
+		GPIOR0 ^= (1 << 0);
+
+		if (GPIOR0 & (1 << 0)) { //tx aan
+			if (sensor == true) {
+				sensoraan = true;
+			}
+			PORTB &= ~(1 << 1); //TXled uit
+		}
+		else { //txled uit
+			if (sensor == false && sensoraan == true) {
+				bt = 600; PORTB |= (1 << 0); //indicatie led aan
+			}
+			sensoraan = false;
+			PORTB |= (1 << 1); //TXled aan	
+		}
+
+		if (bt > 1) {
+			bt--;
+		}
+		else {
+			PORTB &= ~(1 << 0); //led uit
+		}
+	}
+
 }
---
+++ MelDers/LaserSensor/LaserSensor.ino
@@ -0,0 +1,38 @@
+/*
+ Name:		LaserSensor.ino
+ Created:	4/30/2022 11:10:30 AM
+ Author:	gebruiker
+*/
+
+
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X sensor;
+
+void setup()
+{
+	Serial.begin(9600);
+	Wire.begin();
+
+	sensor.setTimeout(500);
+	if (!sensor.init())
+	{
+		Serial.println("Failed to detect and initialize sensor!");
+		while (1) {}
+	}
+
+	// Start continuous back-to-back mode (take readings as
+	// fast as possible).  To use continuous timed mode
+	// instead, provide a desired inter-measurement period in
+	// ms (e.g. sensor.startContinuous(100)).
+	sensor.startContinuous();
+}
+
+void loop()
+{
+	Serial.print(sensor.readRangeContinuousMillimeters());
+	if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+	Serial.println();
+}
